99fbd75e93823f4b6dfa4a00f960323aa9f1d4e6
Princess May Suarez <pmsuarez@gofluent.com>
2018-07-24 17:56:18 +0800

    Test

---
+++ Object Repository/Test/Test.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Test</name>
+   <tag></tag>
+   <elementGuidId>93deef99-5b52-4da4-8c7e-bed80a4ce514</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>Test</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
